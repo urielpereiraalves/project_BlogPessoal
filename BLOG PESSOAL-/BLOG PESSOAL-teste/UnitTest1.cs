1b5798c0dbe46f77fbe0918059dddde9beea4afe
urielpereiraalves <upereiraalves@hotmail.com>
2022-04-27 11:37:58 -0300

    @repouriel [Task: commit inicial] {27/04/2022 11:37}

---
+++ BLOG PESSOAL-/BLOG PESSOAL-teste/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BLOG_PESSOAL_teste
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
