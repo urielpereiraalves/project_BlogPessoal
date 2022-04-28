2f014458dd87f6dc3191d933ae6d4374c2888791
urielpereiraalves <upereiraalves@hotmail.com>
2022-04-28 16:59:02 -0300

    @repouriel [Task:teste de contexto] {28/04/2022}

---
+++ BLOG PESSOAL-teste/Testes/data/BlogPessoalContextoTeste.cs
@@ -0,0 +1,46 @@
+using Microsoft.EntityFrameworkCore;
+using BlogPessoal.src.data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BLOG_PESSOAL_.src.modelos;
+using System.Linq;
+
+namespace BLOGPESSOALTeste.Testes.data
+{
+    [TestClass]
+    public class BlogPessoalContextoTeste
+    {
+        private BlogPessoalContexto _contexto;
+
+        [TestInitialize]
+
+        public void Inicio()
+        {
+            var opt = new DbContextOptionsBuilder<BlogPessoalContexto>()
+                 .UseInMemoryDatabase(databaseName: "db_blogpessoal")
+                 .Options;
+
+            _contexto = new BlogPessoalContexto(opt);
+
+
+
+        }
+
+        [TestMethod]
+        public void InserirNovoUsuarioNoBancoRetornarUsuario()
+        {
+            UsuarioModelo Usuario = new UsuarioModelo();
+
+            Usuario.Nome = "Uriel Pereira";
+            Usuario.Email = "Uriel@email.com";
+            Usuario.Senha = "123456";
+            Usuario.Foto = "Fotolinda";
+
+            _contexto.Usuarios.Add(Usuario);
+
+            _contexto.SaveChanges(); // Commita criação
+
+            Assert.IsNotNull(_contexto.Usuarios.FirstOrDefault(u => u.Email == "Uriel@email.com"));
+
+        }
+    }
+}
